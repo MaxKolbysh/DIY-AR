c30ebc2a398401692fdc222027118f3c8a4e36c1
Kolbyshev, Maksim <int1123875@hotmail.com>
2022-05-23 15:17:31 +0200

    bug with id fix for
    bug with id fix for  sensor count

--- assets/js/elementinterface.js
+++ assets/js/elementinterface.js
@@ -164,7 +164,7 @@
 							
 									elementDataContainer.id= `elementdatacontainer-${hotspotCounter}`;
 									elementDataContainer.classList.add("elementdatacontainer");
-									elementDataContainer.dataset.sensorTopic = document.querySelector(sensorcounter).dataset.sensorTopic;
+									elementDataContainer.dataset.sensorTopic = document.querySelector("#"+sensorcounter).dataset.sensorTopic;
 									document.getElementById(`hotspot-${hotspotCounter}`).appendChild(elementDataContainer);
 									
 									let elementName = document.createElement("div");
